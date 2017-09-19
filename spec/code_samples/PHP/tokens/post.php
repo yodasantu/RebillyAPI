1e2c1ba8e8da3d472f5074ce1e82b1f6aa9c6549
Bogdan <thaddeusbox@gmail.com>
2017-09-19 15:57:18 +0600

    Add better examples for Contacts in related resources (#362)
    * Add examples for new addresses in SDK specs
    
    * Add examples with multiple emails and phoneNumbers

--- spec/code_samples/PHP/tokens/post.php
+++ spec/code_samples/PHP/tokens/post.php
@@ -1,11 +1,37 @@
 $paymentCardTokenForm = new Rebilly\Entities\PaymentCardToken();
-$paymentCardTokenForm->setFirstName('John');
-$paymentCardTokenForm->setLastName('Doe');
-$paymentCardTokenForm->setAddress('1313 Main Street');
-$paymentCardTokenForm->setCity('Gotham');
-$paymentCardTokenForm->setPostalCode('12345');
-$paymentCardTokenForm->setRegion('NY');
-$paymentCardTokenForm->setCountry('US');
+$paymentCardTokenForm->setBillingAddress([
+    'firstName' => 'John',
+    'lastName' => 'Doe',
+    'organization' => 'Test LTD',
+    'address' => 'Test street 5',
+    'address2' => 'Test house 5',
+    'city' => 'New York',
+    'region' => 'Long Island',
+    'country' => 'US',
+    'postalCode' => '123456',
+    'emails' => [
+        [
+            'label' => 'main',
+            'value' => 'johndoe@testemail.com',
+            'primary' => true,
+        ],
+        [
+            'label' => 'secondary',
+            'value' => 'otheremail@testemail.com',
+        ],
+    ],
+    'phoneNumbers' => [
+        [
+            'label' => 'work',
+            'value' => '+123456789',
+            'primary' => true,
+        ],
+        [
+            'label' => 'home',
+            'value' => '+9874654321',
+        ],
+    ],
+]);
 
 $paymentInstrumentForm = new Entities\PaymentInstruments\PaymentCardPaymentInstrument();
 $paymentInstrumentForm->setPan('4111111111111111');
