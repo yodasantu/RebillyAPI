8355835c35a59af20a92b983b63fe30c26e58b71
Adam Altman <adam@rebilly.com>
2018-02-20 20:31:45 -0600

    Add subscription change (#412)
    * Add subscription change
    
    * Adjust property name to lineItems
    
    * Fix type to number
    
    * Add prorated type boolean
    
    * Add preview as flag in request
    
    * Add code sample for subscription change plan

---
+++ spec/code_samples/PHP/subscriptions@{id}@change-plan/post.php
@@ -0,0 +1,12 @@
+$subscriptionChangePlanForm = new Rebilly\Entities\SubscriptionChangePlan();
+$subscriptionChangePlanForm->setPlanId('newPlanId');
+$subscriptionChangePlanForm->setRenewalPolicy('retain');
+$subscriptionChangePlanForm->setPreview(true);
+$subscriptionChangePlanForm->setProrated(true);
+$subscriptionChangePlanForm->setEffectiveTime('2018-02-02 00:00:00');
+
+try {
+    $subscription = $client->subscriptions()->changePlan('subscriptionId', $subscriptionChangePlanForm);
+} catch (UnprocessableEntityException $e) {
+    echo $e->getMessage();
+}
